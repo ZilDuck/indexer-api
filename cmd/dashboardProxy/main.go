cfd563cd1958f1efba6126d3abfa297a470032dc
Dan Tudor <dtudor01@gmail.com>
2021-11-20 00:40:22 +0000

    Direct proxy

--- cmd/dashboardProxy/main.go
+++ cmd/dashboardProxy/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/ZilDuck/indexer-api/generated/dic"
 	"github.com/ZilDuck/indexer-api/internal/config"
 	"github.com/ZilDuck/indexer-api/internal/framework"
 	"github.com/apex/gateway"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/olivere/elastic/v7"
+	"github.com/sha1sum/aws_signing_client"
 	"go.uber.org/zap"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
@@ -46,24 +48,41 @@ func setupRouter() *gin.Engine {
 	r.Use(framework.ErrorHandler)
 
 	r.NoRoute(func(c *gin.Context) {
-		container.GetElastic().Client.Index()
-
-		resp, err := container.GetElastic().Client.PerformRequest(context.Background(), elastic.PerformRequestOptions{
-			Method:      c.Request.Method,
-			Path:        c.Request.RequestURI,
-			Params:      nil,
-			Body:        c.Request.Body,
-			ContentType: "application/x-ndjson",
-			Retrier:     nil,
-			Headers:     nil,
-		})
-
+		awsConfig := config.Get().Aws
+		Swapcreds := credentials.NewStaticCredentials(awsConfig.AccessKey, awsConfig.SecretKey, awsConfig.Token)
+		awsClient, err := aws_signing_client.New(v4.NewSigner(creds), nil, "es", awsConfig.Region)
 		if err != nil {
+			panic(err)
+		}
+
+		baseUrl := "http://search-zilkroad-index-wlmccxpkwz6ps7sohqqjvdphqa.us-east-1.es.amazonaws.com"
+		req, err := http.NewRequest(c.Request.Method, fmt.Sprintf("%s%s", baseUrl, c.Request.RequestURI), c.Request.Body)
+		if err != nil {
+			panic(err)
+		}
+		req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+		resp, err := awsClient.Do(req)
+		if err != nil {
+			zap.L().Error(err.Error())
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			zap.L().Error(err.Error())
 			c.JSON(500, gin.H{"code": 500, "message": err.Error()})
 			return
 		}
 
-		c.JSON(resp.StatusCode, resp.Body)
+		for key, value := range resp.Header {
+			for _, v := range value {
+				c.Header(key, v)
+			}
+		}
+		zap.L().Info(string(body))
+
+		c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), body)
 	})
 
 	return r

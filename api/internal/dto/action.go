681baec327d16c1f2a90781a657a3a012af249eb
dantudor <dtudor01@gmail.com>
2022-04-08 16:32:04 +0100

    Support marketplace actions (disabled)

--- api/internal/dto/action.go
+++ api/internal/dto/action.go
@@ -4,6 +4,13 @@ type NftAction struct {
 	TxID     string `json:"txId"`
 	BlockNum uint64 `json:"blockNum"`
 	Action   string `json:"action"`
-	From     string `json:"from"`
-	To       string `json:"to"`
+
+	From *string `json:"from,omitempty"`
+	To   *string `json:"to,omitempty"`
+
+	Marketplace *string `json:"marketplace,omitempty"`
+	Cost        *uint64 `json:"cost,omitempty"`
+	Fee         *uint64 `json:"fee,omitempty"`
+	Royalty     *uint64 `json:"royalty,omitempty"`
+	Fungible    *string `json:"fungible,omitempty"`
 }
--- api/internal/mapper/action.go
+++ api/internal/mapper/action.go
@@ -3,23 +3,76 @@ package mapper
 import (
 	"github.com/ZilDuck/indexer-api/internal/dto"
 	"github.com/ZilDuck/indexer-api/internal/entity"
+	"strconv"
 )
 
+var supportedActions = []string{"mint", "transfer", "burn"}
+
 func ActionsToDtos(e []entity.NftAction) []dto.NftAction {
-	nfts := make([]dto.NftAction, 0)
+	actions := make([]dto.NftAction, 0)
 	for idx := range e {
-		nfts = append(nfts, ActionToDto(e[idx]))
+		action := ActionToDto(e[idx])
+		if action != nil {
+			actions = append(actions, *action)
+		}
 	}
 
-	return nfts
+	return actions
 }
 
-func ActionToDto(e entity.NftAction) dto.NftAction {
-	return dto.NftAction{
+func ActionToDto(e entity.NftAction) *dto.NftAction {
+	matched := false
+	for _, supportedAction := range supportedActions {
+		if e.Action == supportedAction {
+			matched = true
+		}
+	}
+	if matched == false {
+		return nil
+	}
+
+	a := &dto.NftAction{
 		BlockNum: e.BlockNum,
 		TxID: e.TxID,
 		Action: e.Action,
-		From: e.From,
-		To: e.To,
 	}
+
+	if e.From != "" {
+		a.From = &e.From
+	}
+
+	if e.To != "" {
+		a.To = &e.To
+	}
+
+	if e.Marketplace != "" {
+		a.Marketplace = &e.Marketplace
+	}
+
+	if e.Cost != "" {
+		n, err := strconv.ParseUint(e.Cost, 10, 64)
+		if err == nil {
+			a.Cost = &n
+		}
+	}
+
+	if e.Fee != "" {
+		n, err := strconv.ParseUint(e.Fee, 10, 64)
+		if err == nil {
+			a.Fee = &n
+		}
+	}
+
+	if e.Royalty != "" {
+		n, err := strconv.ParseUint(e.Royalty, 10, 64)
+		if err == nil {
+			a.Royalty = &n
+		}
+	}
+
+	if e.Fungible != "" {
+		a.Fungible = &e.Fungible
+	}
+
+	return a
 }

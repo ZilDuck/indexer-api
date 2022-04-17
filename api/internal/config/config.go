670aaf5e509c2a824d20c61ca68baffe8a334e91
dantudor <dtudor01@gmail.com>
2022-04-17 22:25:27 +0100

    Migrate to viper

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -1,36 +1,34 @@
 package config
 
 import (
+	"fmt"
 	"github.com/ZilDuck/indexer-api/internal/log"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"os"
-	"strconv"
-	"strings"
 )
 
-var config *Config
+var config Config
 
 type Config struct {
 	Env           string
 	Port          int
 	Debug         bool
-	ElasticSearch ElasticSearchConfig
-	DBConfig      DBConfig
-	Aws           AwsConfig
+	ElasticSearch ElasticSearch
+	DB            DB
+	Aws           Aws
 	CdnHost       string
 	AuditDir      string
 	AdminIds      []string
 }
 
-type AwsConfig struct {
+type Aws struct {
 	AccessKey string
 	SecretKey string
-	Token     string
 	Region    string
 }
 
-type ElasticSearchConfig struct {
-	Host        string
+type ElasticSearch struct {
+	Hosts       []string
 	Sniff       bool
 	HealthCheck bool
 	Debug       bool
@@ -38,7 +36,7 @@ type ElasticSearchConfig struct {
 	Password    string
 }
 
-type DBConfig struct {
+type DB struct {
 	Dialect  string
 	Username string
 	Password string
@@ -51,84 +49,25 @@ type DBConfig struct {
 }
 
 func Init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+	viper.SetConfigName("env.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
 	}
 
-	log.NewLogger(Get().Debug)
+	if err := viper.Unmarshal(&config); err != nil {
+		panic("Failed to unmarshal config")
+	}
+
+	_ = os.Setenv("AWS_ACCESS_KEY_ID", config.Aws.AccessKey)
+	_ = os.Setenv("AWS_SECRET_KEY_ID", config.Aws.SecretKey)
+	_ = os.Setenv("AWS_REGION", config.Aws.Region)
+
+	log.NewLogger(config.Debug)
 }
 
-func Get() *Config {
-	if config == nil {
-		config = &Config{
-			Env:   getString("ENV", ""),
-			Port:  getInt("PORT", 8082),
-			Debug: getBool("DEBUG", false),
-			Aws: AwsConfig{
-				AccessKey: getString("AWS_ACCESS_KEY_ID", ""),
-				SecretKey: getString("AWS_SECRET_KEY_ID", ""),
-				Token:     getString("AWS_TOKEN", ""),
-				Region:    getString("AWS_REGION", ""),
-			},
-			ElasticSearch: ElasticSearchConfig{
-				Host:        getString("ELASTIC_SEARCH_HOST", ""),
-				Sniff:       getBool("ELASTIC_SEARCH_SNIFF", false),
-				HealthCheck: getBool("ELASTIC_SEARCH_HEALTH_CHECK", false),
-				Debug:       getBool("ELASTIC_SEARCH_DEBUG", false),
-				Username:    getString("ELASTIC_SEARCH_USERNAME", ""),
-				Password:    getString("ELASTIC_SEARCH_PASSWORD", ""),
-			},
-			DBConfig: DBConfig{
-				Dialect:  getString("DB_DIALECT", ""),
-				Username: getString("DB_USERNAME", ""),
-				Password: getString("DB_PASSWORD", ""),
-				Host:     getString("DB_HOST", ""),
-				Port:     getInt("DB_PORT", 0),
-				Name:     getString("DB_NAME", ""),
-				SslMode:  getString("DB_SSLMODE", ""),
-				Options:  getString("DB_OPTIONS", ""),
-				RootCA:   getString("DB_ROOT_CA", ""),
-			},
-			CdnHost:    getString("CDN_HOST", ""),
-			AuditDir:   getString("AUDIT_DIR", "/app/audit"),
-			AdminIds:   getSlice("ADMIN_IDS", []string{}, ","),
-		}
-	}
+func Get() Config {
 	return config
-}
-
-func getString(key string, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-
-	return defaultValue
-}
-
-func getInt(key string, defaultValue int) int {
-	valStr := getString(key, "")
-	if val, err := strconv.Atoi(valStr); err == nil {
-		return val
-	}
-
-	return defaultValue
-}
-
-func getBool(key string, defaultValue bool) bool {
-	valStr := getString(key, "")
-	if val, err := strconv.ParseBool(valStr); err == nil {
-		return val
-	}
-
-	return defaultValue
-}
-
-func getSlice(key string, defaultVal []string, sep string) []string {
-	valStr := getString(key, "")
-	if valStr == "" {
-		return defaultVal
-	}
-
-	return strings.Split(valStr, sep)
 }
--- api/internal/elastic_search/elastic_search.go
+++ api/internal/elastic_search/elastic_search.go
@@ -4,14 +4,15 @@ import (
 	"github.com/ZilDuck/indexer-api/internal/config"
 	"github.com/olivere/elastic/v7"
 	"go.uber.org/zap"
+	"strings"
 )
 
 type Index struct {
 	Client *elastic.Client
 }
 
-func New(elasticConfig config.ElasticSearchConfig, awsConfig config.AwsConfig) (Index, error) {
-	client, err := newClient(elasticConfig, awsConfig)
+func New(elasticConfig config.ElasticSearch) (Index, error) {
+	client, err := newClient(elasticConfig)
 	if err != nil {
 		zap.L().With(zap.Error(err)).Fatal("ElasticCache: Failed to create client")
 	}
@@ -19,11 +20,9 @@ func New(elasticConfig config.ElasticSearchConfig, awsConfig config.AwsConfig) (
 	return Index{client}, err
 }
 
-func newClient(elasticConfig config.ElasticSearchConfig, awsConfig config.AwsConfig) (*elastic.Client, error) {
-	zap.S().Infof("Using elastic search instance: %s", elasticConfig.Host)
-
+func newClient(elasticConfig config.ElasticSearch) (*elastic.Client, error) {
 	opts := []elastic.ClientOptionFunc{
-		elastic.SetURL(elasticConfig.Host),
+		elastic.SetURL(strings.Join(config.Get().ElasticSearch.Hosts, ",")),
 		elastic.SetSniff(elasticConfig.Sniff),
 		elastic.SetHealthcheck(elasticConfig.HealthCheck),
 	}
